d992bd93ee3cf95e8043bae7c66368132c1be654
unknown <Administrator@asusbook.amulet.aeroflot.ru>
2012-02-01 22:22:39 +0100

    new readme and Greeter

--- Greeter_018.java
+++ Greeter_018.java
@@ -1,20 +1,21 @@
 public class Greeter {
-	private string format
-	public Greeter()
-		this.format=
+	private String format;
+	public Greeter(String format) {
+		this.format=format;
 	}
-	public.void.greet()
-	System.out.println(format + name);
+	public void greet(String name) {
+		System.out.println(format + name);
 	}
 }
 public class HelloWorld {
-	public static void main(S[] args) {
-	Greeter g=new Greeter("Hello, ");
-	for(S a: args) {
-	g.greet(a);
+	public static void main(String[] args) {
+		Greeter g=new Greeter("Hello, ");
+		for(String a: args) {
+			g.greet(a);
+		}
 	}
-	Geeter g=args[0].equals("en")
-	? new Greeter("Hello, ")
-	: new Greeter("Salut, ");
-	}
+	// Greeter g=args[0].equals("en")
+	//? new Greeter("Hello, ")
+	//: new Greeter("Salut, ");
+	//}
 }
