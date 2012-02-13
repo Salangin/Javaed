eeb40ee9619bc9c9dc04f9ba07aeeeb5168ce044
Salangin <salangin@gmail.com>
2012-02-13 13:22:40 +0400

    new class Example2

---
+++ Book/Example2.java
@@ -0,0 +1,11 @@
+//from book
+class Example2 {
+	public static void main(String args[]) {
+		int num; //integer
+		num=100; //100
+		System.out.println("Num= " + num);
+		num = num *2;
+		System.out.print("2Num: ");
+		System.out.println(num);
+	}
+}
